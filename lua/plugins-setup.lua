e3c1086c116aa49e8cf0298f8179a84b626ac5fe
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-17 15:55:40 +0700

    add lines

--- lua/plugins-setup.lua
+++ lua/plugins-setup.lua
@@ -133,6 +133,7 @@ return packer.startup(function(use)
     -- ====================================================
 
     -- Git integration
+
     use {
 		"lewis6991/gitsigns.nvim",
 		requires = { "nvim-lua/plenary.nvim" },

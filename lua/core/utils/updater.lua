312fe2c3f1866704a0c5a1502baa4fe2e47ba04d
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-18 11:19:45 +0700

    self updater: notify about git pull status

--- lua/core/utils/updater.lua
+++ lua/core/utils/updater.lua
@@ -3,7 +3,13 @@ local create_command = vim.api.nvim_create_user_command
 create_command("InitLuaUpdate", function()
 
     -- Update Self
-    git.pull(false)
+    local pull_status, _ = git.pull(false)
+    if pull_status then
+        initlua.notify("InitLua: Repository pulled successfully")
+    else
+        vim.api.nvim_err_writeln "InitLua: Unable to pull repository"
+        return
+    end
 
     -- Update Packer
     initlua.packer.update()

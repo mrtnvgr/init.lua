46ab1dd9583725c23dc7ab78bd70ddb95aaf08e6
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-21 22:47:19 +0700

    noice.nvim: avoid annoying stuff

--- lua/initlua/plugins/ui.lua
+++ lua/initlua/plugins/ui.lua
@@ -6,31 +6,59 @@ return {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
         },
-        opts = {
-            cmdline = {
-                view = "cmdline",
-            },
-
-            messages = {
-                view_search = false,
-            },
-
-            lsp = {
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
+        config = function()
+            require("noice").setup({
+                cmdline = {
+                    view = "cmdline",
                 },
-            },
 
-            presets = {
-                bottom_search = true,
-                command_palette = false,
-                long_message_to_split = true,
-                inc_rename = false,
-                lsp_doc_border = true,
-            },
-        },
+                messages = {
+                    view_search = false,
+                },
+
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+
+                presets = {
+                    bottom_search = true,
+                    command_palette = false,
+                    long_message_to_split = true,
+                    inc_rename = false,
+                    lsp_doc_border = true,
+                },
+
+                routes = {
+                    -- Avoid written messages
+                    {
+                        filter = {
+                            event = "msg_show",
+                            kind = "",
+                            find = "written",
+                        },
+                        opts = { skip = true },
+                    },
+
+                    -- Avoid search messages
+                    {
+                        filter = {
+                            event = "msg_show",
+                            kind = "search_count",
+                        },
+                        opts = { skip = true },
+                    },
+                },
+            })
+
+            -- https://github.com/folke/noice.nvim/wiki/A-Guide-to-Messages#lsp-messages
+            vim.diagnostic.config({
+                update_in_insert = false
+            })
+        end,
     },
 
     {

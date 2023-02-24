1c1665f883f4e01343e79aaf9d74f0d78df9357f
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-24 12:39:57 +0700

    fix indent-o-matic

--- lua/initlua/plugins/indentation-detection.lua
+++ lua/initlua/plugins/indentation-detection.lua
@@ -2,5 +2,15 @@ return {
 	{
 		"Darazaki/indent-o-matic",
 		event = "BufEnter",
+		config = function()
+			require("indent-o-matic").detect() -- Detect on BufEnter
+			vim.cmd([[autocmd! indent_o_matic]]) -- Disable default autocmd
+			vim.api.nvim_create_autocmd({ "BufReadPost", "InsertChange" }, {
+				group = "indent_o_matic",
+				callback = function()
+					require("indent-o-matic").detect()
+				end,
+			})
+		end,
 	},
 }

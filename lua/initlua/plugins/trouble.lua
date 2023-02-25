a06c3cda4dc9466fea40031815b34795c66dcc4b
mrtnvgr <martynovegorOF@yandex.ru>
2023-02-25 15:22:53 +0700

    todo-comments plugin: add fallback search (see folke/todo-comments.nvim#183)

--- lua/initlua/plugins/trouble.lua
+++ lua/initlua/plugins/trouble.lua
@@ -5,8 +5,11 @@ return {
 		dependencies = { "kyazdani42/nvim-web-devicons" },
 		config = true,
 	},
+
 	{
-		"folke/todo-comments.nvim",
+		"mrtnvgr/todo-comments.nvim",
+		-- TODO: https://github.com/folke/todo-comments.nvim/pull/183
+		branch = "fallback_searching",
 		event = "VeryLazy",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {
@@ -15,20 +18,20 @@ return {
 				keyword = "fg",
 				after = "",
 			},
-			search = {
-				command = "grep",
-				args = {
-					"--recursive",
-					"--color=never",
-					"--with-filename",
-					"--line-number",
-					"--binary-files=without-match",
-					"--byte-offset",
-					'--exclude-dir=".*"',
-					"--extended-regexp",
-				},
-				pattern = ".*(KEYWORDS):",
-			},
+			-- search = {
+			-- 	command = "grep",
+			-- 	args = {
+			-- 		"--recursive",
+			-- 		"--color=never",
+			-- 		"--with-filename",
+			-- 		"--line-number",
+			-- 		"--binary-files=without-match",
+			-- 		"--byte-offset",
+			-- 		'--exclude-dir=".*"',
+			-- 		"--extended-regexp",
+			-- 	},
+			-- 	pattern = ".*(KEYWORDS):",
+			-- },
 		},
 	},
 }

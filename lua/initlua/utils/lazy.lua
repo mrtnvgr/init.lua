a787c69f85b17f76a77cf966ae1eeadd11551385
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-01 22:43:27 +0700

    fix lazy autocmd yet again

--- lua/initlua/utils/lazy.lua
+++ lua/initlua/utils/lazy.lua
@@ -3,18 +3,18 @@ initlua.lazy = {}
 function initlua.lazy.update()
 	require("lazy.core.plugin").load()
 
-	vim.api.nvim_create_autocmd("User", {
-		once = true,
-		desc = "Close Lazy window after update",
-		pattern = "LazySync",
-		-- TODO: https://github.com/folke/lazy.nvim/issues/578
-		-- callback = function()
-		-- 	local has_updates = require("lazy.status").has_updates
-		-- 	if not has_updates then
-		-- 		require("lazy.view").close()
-		-- 	end
-		-- end,
-	})
+	-- TODO: https://github.com/folke/lazy.nvim/issues/578
+	-- vim.api.nvim_create_autocmd("User", {
+	-- 	once = true,
+	-- 	desc = "Close Lazy window after update",
+	-- 	pattern = "LazySync",
+	-- 	callback = function()
+	-- 		local has_updates = require("lazy.status").has_updates
+	-- 		if not has_updates then
+	-- 			require("lazy.view").close()
+	-- 		end
+	-- 	end,
+	-- })
 
 	require("lazy").sync({ wait = true })
 end

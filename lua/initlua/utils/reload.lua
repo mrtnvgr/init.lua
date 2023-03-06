9e92f59d0168ed532d49d42f66941f56a114ddc8
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-06 16:20:01 +0700

    reload utils module: fix match (core does not exist anymore)

--- lua/initlua/utils/reload.lua
+++ lua/initlua/utils/reload.lua
@@ -4,7 +4,7 @@ function initlua.reload()
 	local reloaded = true
 
 	for name, _ in pairs(package.loaded) do
-		if name:match("^initlua.core") then
+		if not name:match("^initlua.plugins") then
 			local status, _ = pcall(reload_module, name)
 			local status2, _ = pcall(require, name)
 			if not status or not status2 then

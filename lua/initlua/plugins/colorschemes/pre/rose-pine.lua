516334f680dfeb3fa6c14a3064b51b49ed0061b7
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-07 15:03:55 +0700

    rose-pine colorscheme: fix `Cursor` highlight group

--- lua/initlua/plugins/colorschemes/pre/rose-pine.lua
+++ lua/initlua/plugins/colorschemes/pre/rose-pine.lua
@@ -1,3 +1,7 @@
 require("rose-pine").setup({
 	disable_float_background = true,
+	-- TODO: workaround until https://github.com/rose-pine/neovim/pull/138 will be merged
+	highlight_groups = {
+		Cursor = { bg = "text", fg = "highlight_high" },
+	},
 })

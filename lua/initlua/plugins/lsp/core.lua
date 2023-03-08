23be65c899092a2c641af672ab6cda4d930f872f
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-08 09:47:45 +0700

    lsp: turn off lsp keymaps (which-key handles them already)

--- lua/initlua/plugins/lsp/core.lua
+++ lua/initlua/plugins/lsp/core.lua
@@ -2,16 +2,9 @@ local M = {}
 
 M.lsp = require("lsp-zero").preset({
 	name = "recommended",
-	set_lsp_keymaps = true,
+	set_lsp_keymaps = { preserve_mappings = false },
 	manage_nvim_cmp = false,
 	suggest_lsp_servers = true,
 })
-
-M.lsp.on_attach(function(_, bufnr)
-	local opts = { buffer = bufnr }
-	local bind = vim.keymap.set
-
-	bind("n", "gl", "<CMD>lua vim.diagnostic.open_float()<CR>", opts)
-end)
 
 return M.lsp

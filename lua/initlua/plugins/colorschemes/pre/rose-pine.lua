a3bef3ddaf37330d3cf6f179dc9b5a7868a8906b
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-08 10:59:29 +0700

    rose-pine colorscheme: remove workaround (rose-pine/neovim#138)

--- lua/initlua/plugins/colorschemes/pre/rose-pine.lua
+++ lua/initlua/plugins/colorschemes/pre/rose-pine.lua
@@ -1,7 +1,3 @@
 require("rose-pine").setup({
 	disable_float_background = true,
-	-- TODO: workaround until https://github.com/rose-pine/neovim/pull/138 will be merged
-	highlight_groups = {
-		Cursor = { bg = "text", fg = "highlight_high" },
-	},
 })

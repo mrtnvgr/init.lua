a4c485c3bd0f33f725064c0e1887283294235a07
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-12 18:06:34 +0700

    lsp: improve ltex

--- lua/initlua/plugins/lsp/servers/ltex.lua
+++ lua/initlua/plugins/lsp/servers/ltex.lua
@@ -1,3 +1,33 @@
+local function curbuf_uri()
+	return vim.uri_from_bufnr(vim.api.nvim_get_current_buf())
+end
+
+local function create_autocmds()
+	vim.api.nvim_create_autocmd({ "BufWinEnter", "InsertLeave", "BufWritePost" }, {
+		group = "Initlua",
+		buffer = vim.api.nvim_get_current_buf(),
+		desc = "Check document",
+		callback = function()
+			local client = (vim.lsp.get_active_clients({ name = "ltex" }))[1]
+			client.checkDocument()
+		end,
+	})
+end
+
+local function create_client_functions(client)
+	client.checkDocument = function(uri)
+		client.request(
+			"workspace/executeCommand",
+			{ command = "_ltex.checkDocument", arguments = { { uri = uri or curbuf_uri() } } }
+		)
+	end
+end
+
+local function on_init(client)
+	create_client_functions(client)
+	create_autocmds()
+end
+
 return {
 	settings = {
 		ltex = {
@@ -20,9 +50,11 @@ return {
 					"PASSIVE_VOICE",
 				},
 			},
-			-- Use autocmd for this
-			-- TODO: use manual
-			checkFrequency = "save",
+			checkFrequency = "manual",
 		},
 	},
+	-- Expose checkDocument from ltex client
+	-- Credits: https://github.com/vigoux/ltex-ls.nvim
+	-- TODO(?): This should be removed on v2.x migration
+	on_init = on_init,
 }

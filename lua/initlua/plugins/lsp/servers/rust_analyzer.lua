67609c2d32b269c3fe8b1ec2e14f1738f7287825
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-19 21:20:00 +0700

    rust-analyzer: use clippy instead of check if it is installed

--- lua/initlua/plugins/lsp/servers/rust_analyzer.lua
+++ lua/initlua/plugins/lsp/servers/rust_analyzer.lua
@@ -1,1 +1,23 @@
-return {}
+local check_command = "check"
+
+-- Use clippy if it's installed
+local Job = require("plenary.job")
+Job:new({
+	command = "cargo",
+	args = { "clippy", "-V" },
+	on_exit = function(_, code)
+		if code == 0 then
+			check_command = "clippy"
+		end
+	end,
+}):sync()
+
+return {
+	settings = {
+		["rust-analyzer"] = {
+			check = {
+				command = check_command,
+			},
+		},
+	},
+}

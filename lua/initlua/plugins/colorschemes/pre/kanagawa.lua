35e0309a585ae557bdd139bb3dc9eae23c63f6ff
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-20 08:01:15 +0700

    kanagawa colorscheme: support CR06

--- lua/initlua/plugins/colorschemes/pre/kanagawa.lua
+++ lua/initlua/plugins/colorschemes/pre/kanagawa.lua
@@ -23,7 +23,8 @@ require("kanagawa").setup({
 	overrides = function(colors)
 		local theme = colors.theme
 		return {
-			Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+			-- CR06
+			MsgArea = { link = "Normal" },
 			PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
 			PmenuSbar = { bg = theme.ui.bg_m1 },
 			PmenuThumb = { bg = theme.ui.bg_p2 },

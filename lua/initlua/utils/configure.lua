4633addd00b9a50d7826e2184d4365b8407e1593
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-22 21:15:11 +0700

    configuration: rework

--- lua/initlua/utils/configure.lua
+++ lua/initlua/utils/configure.lua
@@ -28,38 +28,71 @@ initlua.settings = {
 	},
 }
 
+-- TODO: BACK, async_select as local vars here
+
 function initlua.configure.optional_plugins(async_select)
-	for plugin, _ in pairs(initlua.settings.optional_plugins) do
-		local prompt = "Do you want to enable " .. plugin .. "?"
-		local choice = async_select({ "Yes", "No" }, { prompt = prompt })
-		if choice == "Yes" then
-			initlua.settings.optional_plugins[plugin] = true
-			initlua.notify(plugin .. " will be enabled after restart")
+	while true do
+		local plugins = vim.tbl_keys(initlua.settings.optional_plugins)
+		table.sort(plugins)
+		table.insert(plugins, "<-")
+
+		local plugin = async_select(plugins, { prompt = "Optional plugins" })
+		if not plugin or plugin == "<-" then
+			return
 		end
+
+		initlua.configure.optional_plugin(async_select, plugin)
 	end
 end
 
+function initlua.configure.optional_plugin(async_select, plugin)
+	local prompt = "Optional plugin: " .. plugin
+	local choice = async_select({ "Enable", "Disable", "<-" }, { prompt = prompt })
+	if not choice or choice == "<-" then
+		return
+	end
+
+	local boolean = choice == "Enable"
+	local pretty_value = (choice == "Enable" and "enabled") or "disabled"
+
+	initlua.settings.optional_plugins[plugin] = boolean
+	initlua.notify(plugin .. "will be " .. pretty_value .. " after restart")
+end
+
 function initlua.configure.languages(async_select)
-	for name, _ in pairs(initlua.settings.languages) do
-		local prompt = name .. ": integration type"
-		local choices = { "All", "LSP", "Null-ls" }
-		local type = async_select(choices, { prompt = prompt })
+	while true do
+		local languages = vim.tbl_keys(initlua.settings.languages)
+		table.sort(languages)
+		table.insert(languages, "<-")
 
-		prompt = name .. ": " .. type .. " integration state"
-		local state = async_select({ "Enable", "Disable" }, { prompt = prompt })
-
-		local boolean = state == "Enable"
-		local pretty_name = (state == "Enable" and "enabled") or "disabled"
-
-		if type == "LSP" or type == "All" then
-			initlua.settings.languages[name].lsp_enabled = boolean
-			initlua.notify(name .. ": LSP integration was " .. pretty_name)
+		local language = async_select(languages, { prompt = "Languages" })
+		if not language or language == "<-" then
+			return
 		end
 
-		if type == "Formatters, linters, etc..." or type == "All" then
-			initlua.settings.languages[name].null_ls_enabled = boolean
-			initlua.notify(name .. ": null-ls integration was " .. pretty_name)
-		end
+		initlua.configure.language(async_select, language)
+	end
+end
+
+function initlua.configure.language(async_select, language)
+	local prompt = language .. ": integration type"
+	local choices = { "All", "LSP", "Null-ls" }
+	local type = async_select(choices, { prompt = prompt })
+
+	prompt = language .. ": " .. type .. " integration state"
+	local state = async_select({ "Enable", "Disable" }, { prompt = prompt })
+
+	local boolean = state == "Enable"
+	local pretty_name = (state == "Enable" and "enabled") or "disabled"
+
+	if type == "LSP" or type == "All" then
+		initlua.settings.languages[language].lsp_enabled = boolean
+		initlua.notify(language .. ": LSP integration was " .. pretty_name)
+	end
+
+	if type == "Formatters, linters, etc..." or type == "All" then
+		initlua.settings.languages[language].null_ls_enabled = boolean
+		initlua.notify(language .. ": null-ls integration was " .. pretty_name)
 	end
 end
 
@@ -70,11 +103,18 @@ function initlua.configure.all()
 	end, 3)
 
 	async.void(function()
-		initlua.configure.optional_plugins(async_select)
+		local looping_here = true
+		while looping_here do
+			local opts = { "Optional Plugins", "Language Integrations", "Quit" }
+			local choice = async_select(opts, { prompt = "Select options" })
 
-		local ok = async_select({ "Yes", "No" }, { prompt = "Do you want to setup language integrations?" })
-		if ok then
-			initlua.configure.languages(async_select)
+			if choice == "Optional Plugins" then
+				initlua.configure.optional_plugins(async_select)
+			elseif choice == "Language Integrations" then
+				initlua.configure.languages(async_select)
+			else
+				looping_here = false
+			end
 		end
 	end)()
 end

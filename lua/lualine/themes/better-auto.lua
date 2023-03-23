cbaeb17e87c50bf4437e074dcd8d2283e8d90a8c
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-23 11:16:23 +0700

    improve lualine theme

--- lua/lualine/themes/better-auto.lua
+++ lua/lualine/themes/better-auto.lua
@@ -1,6 +1,11 @@
 package.loaded["lualine.themes.auto"] = nil
 local auto = require("lualine.themes.auto")
 
+for key, value in pairs(auto) do
+	if value.c ~= nil and value.c.bg ~= nil then
+		auto[key].c.bg = "NONE"
+	end
+end
 auto.normal.c.bg = "NONE"
 
 return auto

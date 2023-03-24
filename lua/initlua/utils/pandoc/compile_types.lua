bd5e0422d1220fbb24734b90b7a433dab4b0c31e
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-24 22:59:38 +0700

    pandoc pdf presentation integration: migrate to lualatex and metropolis theme

--- lua/initlua/utils/pandoc/compile_types.lua
+++ lua/initlua/utils/pandoc/compile_types.lua
@@ -1,15 +1,15 @@
 function initlua.pandoc.compile_pdf_presentation()
-	initlua.pandoc.compile(
-		"pdf",
-		"beamer",
-		{ "-V", "colortheme:metropolis", "-V", "mainfont:Cascadia Mono", "-V", "fontenc=T2A" },
-		function(output)
-			if vim.fn.executable("zathura") == 0 then
-				return
-			end
-
-			local Job = require("plenary.job")
-			Job:new({ command = "zathura", args = { output } }):start()
+	initlua.pandoc.compile("pdf", "beamer", {
+		"--pdf-engine=lualatex",
+		"--dpi=300",
+		"-V",
+		"theme=metropolis",
+	}, function(output)
+		if vim.fn.executable("zathura") == 0 then
+			return
 		end
-	)
+
+		local Job = require("plenary.job")
+		Job:new({ command = "zathura", args = { output } }):start()
+	end)
 end

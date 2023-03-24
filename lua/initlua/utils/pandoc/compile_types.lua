026f8fc8239fc4e6e8768a126b4b046d3be22c2d
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-24 13:05:11 +0700

    pandoc pdf presentation: set fonttheme to professionalfonts

--- lua/initlua/utils/pandoc/compile_types.lua
+++ lua/initlua/utils/pandoc/compile_types.lua
@@ -1,10 +1,15 @@
 function initlua.pandoc.compile_pdf_presentation()
-	initlua.pandoc.compile("pdf", "beamer", { "-V", "colortheme:metropolis" }, function(output)
-		if vim.fn.executable("zathura") == 0 then
-			return
-		end
+	initlua.pandoc.compile(
+		"pdf",
+		"beamer",
+		{ "-V", "colortheme:metropolis", "-V", "fonttheme:professionalfonts" },
+		function(output)
+			if vim.fn.executable("zathura") == 0 then
+				return
+			end
 
-		local Job = require("plenary.job")
-		Job:new({ command = "zathura", args = { output } }):start()
-	end)
+			local Job = require("plenary.job")
+			Job:new({ command = "zathura", args = { output } }):start()
+		end
+	)
 end

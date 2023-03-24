4ad419a5f8fcd7ef4070390f48b9b804e4f1b510
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-24 21:43:22 +0700

    pandoc integration: add support for other languages

--- lua/initlua/utils/pandoc/compile_types.lua
+++ lua/initlua/utils/pandoc/compile_types.lua
@@ -2,7 +2,7 @@ function initlua.pandoc.compile_pdf_presentation()
 	initlua.pandoc.compile(
 		"pdf",
 		"beamer",
-		{ "-V", "colortheme:metropolis", "-V", "fonttheme:professionalfonts" },
+		{ "-V", "colortheme:metropolis", "-V", "mainfont:Cascadia Mono", "-V", "fontenc=T2A" },
 		function(output)
 			if vim.fn.executable("zathura") == 0 then
 				return

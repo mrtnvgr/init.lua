33a12ddca5c2bd770be1d3edb59f6552c6da1c35
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-26 19:27:34 +0700

    rust language integration: improve clippy defaults

--- lua/initlua/plugins/lsp/servers/rust_analyzer.lua
+++ lua/initlua/plugins/lsp/servers/rust_analyzer.lua
@@ -1,4 +1,5 @@
 local check_command = "check"
+local extraArgs = {}
 
 -- Use clippy if it's installed
 local Job = require("plenary.job")
@@ -8,6 +9,14 @@ Job:new({
 	on_exit = function(_, code)
 		if code == 0 then
 			check_command = "clippy"
+			extraArgs = {
+				"--",
+				"-Wclippy::pedantic",
+				"-Wclippy::nursery",
+				-- "-Wclippy::cargo",
+				"-Wclippy::unwrap_used",
+				"-Wclippy::expect_used",
+			}
 		else
 			initlua.notify("rust-clippy is not installed", vim.log.levels.WARN)
 		end
@@ -19,6 +28,7 @@ return {
 		["rust-analyzer"] = {
 			check = {
 				command = check_command,
+				extraArgs = extraArgs,
 			},
 		},
 	},

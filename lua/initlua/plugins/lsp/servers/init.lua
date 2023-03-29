13c9492c6e5ddb5a39e0b0a745e099bcbb5d8446
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-30 01:59:54 +0700

    lsp: fix ensure_installed

--- lua/initlua/plugins/lsp/servers/init.lua
+++ lua/initlua/plugins/lsp/servers/init.lua
@@ -1,32 +1,13 @@
 local M = {}
 
 function M.get_servers()
-	-- Require plenary modules
-	local plenary_path = require("plenary.path")
-	local plenary_scandir = require("plenary.scandir")
-
-	-- Get servers path
-	local path = plenary_path:new(initlua.install_path, "lua", "initlua", "plugins", "lsp", "servers")
-	local path_sep = path._sep
-	local servers = plenary_scandir.scan_dir(path.filename)
-
-	local valid_servers = {}
-
-	for _, server in ipairs(servers) do
-		-- Get filename from path
-		server = vim.split(server, path_sep, { plain = true, trimempty = true })
-		server = server[#server]
-
-		-- Remove ".lua" prefix from filenames
-		server = server:gsub(".lua$", "")
-
-		-- This file is not a valid lsp server
-		if not server:match("init") then
-			table.insert(valid_servers, server)
+	local servers = {}
+	for _, language in pairs(initlua.settings.languages) do
+		if language.lsp_enabled then
+			servers = vim.list_extend(servers, language.lsp_servers)
 		end
 	end
-
-	return valid_servers
+	return servers
 end
 
 function M.configure()

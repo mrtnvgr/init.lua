d4e43815427c9f04f86312aa05dfde0b66f559e7
mrtnvgr <martynovegorOF@yandex.ru>
2023-04-07 18:00:06 +0700

    feat(lsp): improve lua_ls configuration

--- lua/initlua/plugins/lsp/servers/lua_ls.lua
+++ lua/initlua/plugins/lsp/servers/lua_ls.lua
@@ -1,11 +1,15 @@
 return {
 	settings = {
-		lua = {
+		Lua = {
 			-- using null-ls formatting instead
 			format = { enable = false },
 			telemetry = { enable = false },
 			diagnostics = {
 				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
 			},
 		},
 	},

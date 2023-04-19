c3add1fcc92c6180b578a15b5425dfca7e5bc8d8
Egor Martynov <martynovegorOF@yandex.ru>
2023-04-19 09:04:52 +0700

    feat(langmapper): add leap support

--- lua/initlua/plugins/langmapper.lua
+++ lua/initlua/plugins/langmapper.lua
@@ -1,5 +1,14 @@
 return {
 	"Wansmer/langmapper.nvim",
 	event = "VeryLazy",
-	config = true,
+	config = function()
+		require("langmapper").setup()
+
+		require("leap.util")["get-input"] = function()
+			local ok, ch = pcall(vim.fn.getcharstr)
+			if ok and ch ~= vim.api.nvim_replace_termcodes("<esc>", true, false, true) then
+				return require("langmapper.utils").translate_keycode(ch, "default", "ru")
+			end
+		end
+	end,
 }

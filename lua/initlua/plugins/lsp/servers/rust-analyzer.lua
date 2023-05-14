c76ee3e47f2e89d2b7f70b78707c91f75c0f8b19
mrtnvgr <martynovegorOF@yandex.ru>
2023-05-14 10:12:43 +0700

    feat(rust): turn on more clippy lints

--- lua/initlua/plugins/lsp/servers/rust-analyzer.lua
+++ lua/initlua/plugins/lsp/servers/rust-analyzer.lua
@@ -19,8 +19,30 @@ Job:new({
 					"--",
 					"-Wclippy::pedantic",
 					"-Wclippy::nursery",
+
+					"-Wclippy::create_dir",
+					"-Wclippy::empty_structs_with_brackets",
+					"-Wclippy::filetype_is_file",
+					-- "-Wclippy::format_push_string",
+					"-Wclippy::get_unwrap",
+					"-Wclippy::indexing_slicing",
+					"-Wclippy::impl_trait_in_params",
+					"-Wclippy::lossy_float_literal",
+					"-Wclippy::str_to_string",
+					"-Wclippy::string_add",
+					"-Wclippy::string_to_string",
+					"-Wclippy::suspicious_xor_used_as_pow",
+					"-Wclippy::unneeded_field_pattern",
+					"-Wclippy::unnecessary_self_imports",
+					"-Wclippy::unseparated_literal_suffix",
+					-- "-Wclippy::wildcard_enum_match_arm",
+
 					-- 	"-Wclippy::unwrap_used",
 					-- 	"-Wclippy::expect_used",
+
+					"-Aclippy::missing_errors_doc",
+					"-Aclippy::missing_panics_doc",
+					"-Aclippy::must_use_candidate",
 				},
 			}
 		else

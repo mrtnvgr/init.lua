104932bbf5420d3a0aaed11c7764c7adc6881b79
mrtnvgr <martynovegorOF@yandex.ru>
2023-07-19 10:52:13 +0700

    feat: leap -> flash

--- lua/initlua/plugins/langmapper.lua
+++ lua/initlua/plugins/langmapper.lua
@@ -5,12 +5,5 @@ return {
 		local langmapper = require("langmapper")
 		langmapper.setup()
 		langmapper.hack_get_keymap()
-
-		require("leap.util")["get-input"] = function()
-			local ok, ch = pcall(vim.fn.getcharstr)
-			if ok and ch ~= vim.api.nvim_replace_termcodes("<esc>", true, false, true) then
-				return require("langmapper.utils").translate_keycode(ch, "default", "ru")
-			end
-		end
 	end,
 }
